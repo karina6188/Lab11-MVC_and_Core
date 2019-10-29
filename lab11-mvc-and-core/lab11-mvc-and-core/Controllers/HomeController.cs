9e3f74d98612b0171835786cb731306c09b0ba04
Karina Chen <karina6188@gmail.com>
2019-10-29 10:08:05 -0700

    add class for HomeController and Year

---
+++ lab11-mvc-and-core/lab11-mvc-and-core/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+/// <summary>
+/// Summary description for HomeController
+/// </summary>
+public class HomeController : Controller
+{
+    [HttpGet]
+	public ViewResult Index()
+	{
+        return View();
+	}
+
+    [HttpPost]
+    public IActionResult Index(int fromYear, int toYear)
+    {
+        Year year = new Year(fromYear, toYear);
+        return RedirectToAction("Results", year);
+    }
+
+    public ViewResult Results(Year year)
+    {
+        return View(year);
+    }
+}
---
+++ lab11-mvc-and-core/lab11-mvc-and-core/Models/Year.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Year
+/// </summary>
+public class Year
+{
+    public int FromYear { get; set; }
+    public int ToYear { get; set; }
+
+    public Year(int fromYear, int toYear)
+    {
+        FromYear = fromYear;
+        ToYear = toYear;
+    }
+	public Year()
+	{
+
+	}
+}
